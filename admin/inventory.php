fc132b5eee6f26a440cbdfa4a5eb0b46b653ed96
Kentz777 <116345480+Kentz777@users.noreply.github.com>
2023-04-02 22:17:07 +0800

    new modification

--- admin/inventory.php
+++ admin/inventory.php
@@ -15,23 +15,23 @@
 								<th class="text-center">Stock Available</th>
 							</thead>
 							<tbody>
-							<?php 
+								<?php
 								$i = 1;
 								$supply = $conn->query("SELECT * FROM supply_list order by name asc");
-								while($row=$supply->fetch_assoc()):
+								while ($row = $supply->fetch_assoc()) :
 									$sup_arr[$row['id']] = $row['name'];
-								$inn = $conn->query("SELECT sum(qty) as inn FROM inventory where stock_type = 1 and supply_id = ".$row['id']);
-								$inn = $inn && $inn->num_rows > 0 ? $inn->fetch_array()['inn'] : 0;
-								$out = $conn->query("SELECT sum(qty) as `out` FROM inventory where stock_type = 2 and supply_id = ".$row['id']);
-								$out = $out && $out->num_rows > 0 ? $out->fetch_array()['out'] : 0;
-								$available = $inn - $out;
-							?>
-								<tr>
-									<td class="text-center"><?php echo $i++ ?></td>
-									<td class=""><?php echo $row['name'] ?></td>
-									<td class="text-right"><?php echo $available ?></td>
-								</tr>
-							<?php endwhile; ?>
+									$inn = $conn->query("SELECT sum(qty) as inn FROM inventory where stock_type = 1 and supply_id = " . $row['id']);
+									$inn = $inn && $inn->num_rows > 0 ? $inn->fetch_array()['inn'] : 0;
+									$out = $conn->query("SELECT sum(qty) as `out` FROM inventory where stock_type = 2 and supply_id = " . $row['id']);
+									$out = $out && $out->num_rows > 0 ? $out->fetch_array()['out'] : 0;
+									$available = $inn - $out;
+								?>
+									<tr>
+										<td class="text-center"><?php echo $i++ ?></td>
+										<td class=""><?php echo $row['name'] ?></td>
+										<td class="text-right"><?php echo $available ?></td>
+									</tr>
+								<?php endwhile; ?>
 							</tbody>
 						</table>
 					</div>
@@ -49,30 +49,25 @@
 								<th class="text-center">Date</th>
 								<th class="text-center">Supply Name</th>
 								<th class="text-center">Qty</th>
-								<th class="text-center">Type</th>
 								<th class="text-center"></th>
 							</thead>
 							<tbody>
-							<?php 
+								<?php
 								$i = 1;
 								$inventory = $conn->query("SELECT * FROM inventory order by id desc");
-								while($row=$inventory->fetch_assoc()):
-							?>
-								<tr>
-									<td class="text-center"><?php echo date("Y-m-d",strtotime($row['date_created'])) ?></td>
-									<td class=""><?php echo $sup_arr[$row['supply_id']] ?></td>
-									<td class="text-right"><?php echo $row['qty'] ?></td>
-									<?php if($row['stock_type'] == 1): ?>
-										<td class="text-center"><span class="badge badge-primary"> IN </span></td>
-									<?php else: ?>
-										<td class="text-center"><span class="badge badge-secondary"> Used </span></td>
-									<?php endif; ?>
-									<td>
-										<button type="button" class="btn btn-sm btn-outline-primary edit_stock" data-id="<?php echo $row['id'] ?>"><i class="fa fa-edit"></i></button>
-										<button type="button" class="btn btn-sm btn-outline-danger delete_stock" data-id="<?php echo $row['id'] ?>"><i class="fa fa-trash"></i></button>
-									</td>
-								</tr>
-							<?php endwhile; ?>
+								while ($row = $inventory->fetch_assoc()) :
+								?>
+									<tr>
+										<td class="text-center"><?php echo date("Y-m-d", strtotime($row['date_created'])) ?></td>
+										<td class=""><?php echo $sup_arr[$row['supply_id']] ?></td>
+										<td class="text-right"><?php echo $row['qty'] ?></td>
+
+										<td>
+											<button type="button" class="btn btn-sm btn-outline-primary edit_stock" data-id="<?php echo $row['id'] ?>"><i class="fa fa-edit"></i></button>
+											<button type="button" class="btn btn-sm btn-outline-danger delete_stock" data-id="<?php echo $row['id'] ?>"><i class="fa fa-trash"></i></button>
+										</td>
+									</tr>
+								<?php endwhile; ?>
 							</tbody>
 						</table>
 					</div>
@@ -85,28 +80,31 @@
 
 <script>
 	$('table').dataTable()
-	$('#manage-supply').click(function(){
-		uni_modal("Manage Supply","manage_inv.php")
+	$('#manage-supply').click(function() {
+		uni_modal("Manage Supply", "manage_inv.php")
 	})
-	$('.edit_stock').click(function(){
-		uni_modal("Manage Supply","manage_inv.php?id="+$(this).attr('data-id'))
+	$('.edit_stock').click(function() {
+		uni_modal("Manage Supply", "manage_inv.php?id=" + $(this).attr('data-id'))
 	})
-	$('.delete_stock').click(function(){
-		_conf("Are you sre to remove this data from list?","delete_stock",[$(this).attr('data-id')])
+	$('.delete_stock').click(function() {
+		_conf("Are you sre to remove this data from list?", "delete_stock", [$(this).attr('data-id')])
 	})
 	$('#laundry-list').dataTable()
-	function delete_stock($id){
+
+	function delete_stock($id) {
 		start_load()
 		$.ajax({
-			url:'ajax.php?action=delete_inv',
-			method:'POST',
-			data:{id:$id},
-			success:function(resp){
-				if(resp==1){
-					alert_toast("Data successfully deleted",'success')
-					setTimeout(function(){
+			url: 'ajax.php?action=delete_inv',
+			method: 'POST',
+			data: {
+				id: $id
+			},
+			success: function(resp) {
+				if (resp == 1) {
+					alert_toast("Data successfully deleted", 'success')
+					setTimeout(function() {
 						location.reload()
-					},1500)
+					}, 1500)
 
 				}
 			}
