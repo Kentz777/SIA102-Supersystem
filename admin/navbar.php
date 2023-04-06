f94fa843575c7d26c19f0773750b521e8e11c992
Kentz777 <116345480+Kentz777@users.noreply.github.com>
2023-04-07 06:32:05 +0800

    QR

--- admin/navbar.php
+++ admin/navbar.php
@@ -9,6 +9,9 @@
 		<a href="index.php?page=categories" class="nav-item nav-categories"><span class='icon-field'><i class="fa fa-list"></i></span>Category List</a>
 		<a href="index.php?page=menu" class="nav-item nav-menu"><span class='icon-field'><i class="fa fa-list"></i></span>Products</a>
 
+		<?php $path = "./QR/index" ?>
+		<a href="index.php?page=<?php echo htmlspecialchars($path, ENT_QUOTES); ?>" class="nav-item nav-inventory"><span class='icon-field'><i class="fa fa-list-alt"></i></span> QR Generator</a>
+
 		<a href="index.php?page=food_inventory" class="nav-item nav-foodinventory"><span class='icon-field'><i class="fa fa-archive"></i></span>Product Inventory</a>
 
 		<?php if ($_SESSION['login_type'] == 1) : ?>
@@ -22,9 +25,8 @@
 				<a href="index.php?page=laundry_category" class="nav-item nav-categories"><span class='icon-field'><i class="fa fa-list"></i></span> Laundry Category</a>
 				<a href="index.php?page=supply" class="nav-item nav-supply"><span class='icon-field'><i class="fa fa-boxes"></i></span> Supply List</a>
 				<a href="index.php?page=inventory" class="nav-item nav-inventory"><span class='icon-field'><i class="fa fa-list-alt"></i></span> Inventory</a>
-				<a href="index.php?page=qr_gen" class="nav-item nav-inventory"><span class='icon-field'><i class="fa fa-list-alt"></i></span> QR Generator</a>
+				
 			</div>
-
 			<a href="index.php?page=site_settings" class="nav-item nav-site_settings"><span class='icon-field'><i class="fa fa-cogs"></i></span>Settings</a>
 			<a href="index.php?page=reports" class="nav-item nav-reports"><span class='icon-field'><i class="fa fa-th-list"></i></span> Reports</a>
 
