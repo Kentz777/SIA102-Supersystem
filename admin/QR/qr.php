f5a482738225d94f2460bc031e0f94a3a1accb39
Kentz777 <116345480+Kentz777@users.noreply.github.com>
2023-05-02 23:50:36 +0800

    qr ui clean

--- admin/QR/qr.php
+++ admin/QR/qr.php
@@ -25,7 +25,7 @@ if(isset($_POST['submit']) ) {
     echo '<div class="qr-field">
                 <h2 style="text-align:center">QR Code Result: </h2>
                 <center>
-                    <div class="qrframe" style="border:2px solid black; width:210px; height:210px;">
+                    <div class="qrframe">
                             <img src="QR/temp/'. $filename .'.png" style="width:200px; height:200px;"><br>
                     </div>
                     <a class="btn btn-primary submitBtn" style="width:210px; margin:5px 0;" href="QR/download.php?file='. $filename .'.png">Download QR Code</a>
@@ -37,29 +37,35 @@ if(isset($_POST['submit']) ) {
 <html lang="en-US">
     <head>
     <title>(QR) Code Generator</title>
-    <link rel="stylesheet" href="QR/libs/css/bootstrap.min.css">
-    <link rel="stylesheet" href="libs/style.css">
+   
     <script src="libs/navbarclock.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/qrcode@1.4.4/qrcode.min.js"></script>
+
+    <style>
+        .myoutput {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 200px;
+
+}
+
+
+
+    </style>
     </head>
     <body onload="startTime()">
-        <nav class="navbar-inverse" style="background-color: blue" role="navigation">
-            <a href="qr.php"><h1 style="color:white">QR Code Generator</h1></a>
-            <div id="clockdate">
-                <div class="clockdate-wrapper">
-                    <div id="clock"></div>
-                    <div id="date"><?php echo date('l, F j, Y'); ?></div>
-                </div>
-            </div>
-        </nav>
+     <center>
         <div class="myoutput">
-            <h3><strong>(QR) Code Generator</strong></h3>
+            <div class="container">
+            <h3 style="color:white;"><strong>Generate Room QR</strong></h3>
             <div class="input-field">
-                <h3>Please Fill-out All Fields</h3>
+         
                 <form id="myForm" method="post">
                     <div class="form-group">
-                        <label>Message</label>
-                        <input type="text" class="form-control" name="msg" style="width:20em;" value="<?php echo @$body; ?>" required pattern="[a-zA-Z0-9 .]+" placeholder="Enter your message">
+                        <label style="color:white;">Enter Room No.</label>
+                        <input type="text" class="form-control" name="msg" style="width:20em;" value="<?php echo @$body; ?>" required pattern="[a-zA-Z0-9 .]+" placeholder="Room#">
                     </div>
                     <div class="form-group">
                         <input type="submit" name="submit" class="btn btn-primary submitBtn" style="width:20em; margin:0;" />
@@ -69,9 +75,11 @@ if(isset($_POST['submit']) ) {
             <div id="qrcode" style="text-align:center; margin-top: 20px;"></div>
             <div class = "dllink" style="text-align:center;margin:-100px 0px 
 ">
-				<h4>www.itsourcecode.com</h4>
+				
 			</div>
+</div>
 		</div>
+</center>
 	</body>
 	<footer></footer>
 </html>
